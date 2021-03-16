5036ced3daec469ce6d2f09b779951f5569a9a3b
wantedly210 <wantedly210@wantedly210.local>
2021-03-16 14:11:59 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # how to set up
 `cd ~`
 `sh ./dotfiles/setup.sh`
+
+# what setup.sh does
+- install .tmux.conf and create symbolic link to it
+- install .vimrc and create symbolic link to it
+- install powerline fonts
--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
+# change default shell to zsh
+chsh -s /bin/zsh
+
+# create symbolic link
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# install zprezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# install poweline font
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh
+cd .. && rm -rf fonts
